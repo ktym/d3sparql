a1e7296bdf8768fe62b23b0626f4b309faf2359a
Francesco Strozzi <francesco.strozzi@gmail.com>
2014-11-13 13:33:54 +0900

    added information in the comment to download worldmap.json file

--- d3sparql.js
+++ d3sparql.js
@@ -1538,6 +1538,7 @@ d3sparql.treemapzoom = function(json, config) {
 			"radius": 5 // circles radius
 			"color": #FF3333 // circles colors
 			"map": "map/world-50m.json" // default location of GeoMap JSON
+                                  // https://github.com/mbostock/topojson/blob/master/examples/world-50m.json
 	} 
   Synopsis:
     d3sparql.query(endpoint, sparql, render)

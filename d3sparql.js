24cdb7e889d04eefacf4019010cf83507c8b839b
Toshiaki Katayama <k@bioruby.org>
2015-05-21 22:48:39 +0900

    Removed testing code for treemapzoom()

--- d3sparql.js
+++ d3sparql.js
@@ -1564,11 +1564,55 @@ d3sparql.treemap = function(json, config) {
 
   CSS/SVG:
     <style>
+#result {
+  width: 960px;
+  height: 500px;
+  background: #ddd;
+}
+
+text {
+  pointer-events: none;
+}
+
+.grandparent text {
+  font-weight: bold;
+}
+
+rect {
+  fill: none;
+  stroke: #fff;
+}
+
+rect.parent,
+.grandparent rect {
+  stroke-width: 2px;
+}
+
+.grandparent rect {
+  fill: orange;
+}
+
+.grandparent:hover rect {
+  fill: #ee9700;
+}
+
+.children rect.parent,
+.grandparent rect {
+  cursor: pointer;
+}
+
+.children rect.parent {
+  fill: #bbb;
+  fill-opacity: .5;
+}
+
+.children:hover rect.child {
+  fill: #bbb;
+}
     </style>
 */
 d3sparql.treemapzoom = function(json, config) {
   var tree = d3sparql.tree(json, config)
-//  d3.select("#json").html(JSON.stringify(tree))
 
   var opts = {
     "width":    config.width    || 800,
@@ -1614,15 +1658,7 @@ d3sparql.treemapzoom = function(json, config) {
     .attr("y", 6 - opts.margin.top)
     .attr("dy", ".75em")
 
-//d3.json("cache/zoomabletreemap.json.size", function(tree) {
-//d3.json("cache/flare.json", function(tree) {
-//console.log(tree)
-
   initialize(tree)
-//  accumulate(tree)
-
-d3.select("#json").html(JSON.stringify(tree))
-
   layout(tree)
   display(tree)
 
@@ -1631,14 +1667,6 @@ d3.select("#json").html(JSON.stringify(tree))
     tree.dx = width
     tree.dy = height
     tree.depth = 0
-  }
-
-  // Aggregate the values for internal nodes. This is normally done by the
-  // treemap layout, but not here because of our custom implementation.
-  function accumulate(d) {
-    return d.children
-        ? d.value = d.children.reduce(function(p, v) { return p + accumulate(v); }, 0)
-        : d.value;
   }
 
   // Compute the treemap layout recursively such that each group of siblings
@@ -1753,204 +1781,6 @@ d3.select("#json").html(JSON.stringify(tree))
         ? name(d.parent) + "." + d.name
         : d.name
   }
-
-//}) // d3.json
-
-}
-
-/* backup */
-d3sparql.treemapzoomback = function(json, config) {
-//  var tree = d3sparql.tree(json, config)
-//  d3.select("#json").html(JSON.stringify(tree))
-
-  var opts = {
-    "width":    config.width    || 800,
-    "height":   config.height   || 500,
-    "margin":   config.margin   || {top: 20, right: 0, bottom: 0, left: 0},
-    "selector": config.selector || "#result"
-  }
-
-  var width  = opts.width - opts.margin.left - opts.margin.right
-  var height = opts.height - opts.margin.top - opts.margin.bottom
-
-  var formatNumber = d3.format(",d")
-  var transitioning
-
-  var x = d3.scale.linear().domain([0, width]).range([0, width])
-  var y = d3.scale.linear().domain([0, height]).range([0, height])
-
-  var treemap = d3.layout.treemap()
-    .children(function(d, depth) { return depth ? null : d._children })
-    .sort(function(a, b) { return a.value - b.value })
-    .ratio(height / width * 0.5 * (1 + Math.sqrt(5)))
-    .round(false)
-
-  var svg = d3.select(opts.selector).html("").append("div").append("svg")
-    .attr("width", opts.width)
-    .attr("height", opts.height)
-    .style("margin-left", -opts.margin.left + "px")
-    .style("margin.right", -opts.margin.right + "px")
-    .append("g")
-    .attr("transform", "translate(" + opts.margin.left + "," + opts.margin.top + ")")
-    .style("shape-rendering", "crispEdges")
-
-  var grandparent = svg.append("g")
-    .attr("class", "grandparent")
-
-  grandparent.append("rect")
-    .attr("y", -opts.margin.top)
-    .attr("width", width)
-    .attr("height", opts.margin.top)
-
-  grandparent.append("text")
-    .attr("x", 6)
-    .attr("y", 6 - opts.margin.top)
-    .attr("dy", ".75em")
-
-//d3.json("cache/zoomabletreemap.json.size", function(tree) {
-d3.json("cache/flare.json", function(tree) {
-console.log(tree)
-
-  initialize(tree)
-  accumulate(tree)
-
-d3.select("#json").html(JSON.stringify(tree))
-
-  layout(tree)
-  display(tree)
-
-  function initialize(tree) {
-    tree.x = tree.y = 0
-    tree.dx = width
-    tree.dy = height
-    tree.depth = 0
-  }
-
-  // Aggregate the values for internal nodes. This is normally done by the
-  // treemap layout, but not here because of our custom implementation.
-  // We also take a snapshot of the original children (_children) to avoid
-  // the children being overwritten when when layout is computed.
-  function accumulate(d) {
-    return (d._children = d.children)
-        ? d.value = d.children.reduce(function(p, v) { return p + accumulate(v); }, 0)
-        : d.value;
-  }
-
-  // Compute the treemap layout recursively such that each group of siblings
-  // uses the same size (1×1) rather than the dimensions of the parent cell.
-  // This optimizes the layout for the current zoom state. Note that a wrapper
-  // object is created for the parent node for each group of siblings so that
-  // the parent’s dimensions are not discarded as we recurse. Since each group
-  // of sibling was laid out in 1×1, we must rescale to fit using absolute
-  // coordinates. This lets us use a viewport to zoom.
-  function layout(d) {
-    if (d._children) {
-      treemap.nodes({_children: d._children})
-      d._children.forEach(function(c) {
-        c.x = d.x + c.x * d.dx
-        c.y = d.y + c.y * d.dy
-        c.dx *= d.dx
-        c.dy *= d.dy
-        c.parent = d
-        layout(c)
-      })
-    }
-  }
-
-  function display(d) {
-    grandparent
-      .datum(d.parent)
-      .on("click", transition)
-      .select("text")
-      .text(name(d))
-
-    var g1 = svg.insert("g", ".grandparent")
-      .datum(d)
-      .attr("class", "depth")
-
-    var g = g1.selectAll("g")
-      .data(d._children)
-      .enter()
-      .append("g")
-
-    g.filter(function(d) { return d._children })
-      .classed("children", true)
-      .on("click", transition)
-
-    g.selectAll(".child")
-      .data(function(d) { return d._children || [d] })
-      .enter()
-      .append("rect")
-      .attr("class", "child")
-      .call(rect)
-
-    g.append("rect")
-      .attr("class", "parent")
-      .call(rect)
-      .append("title")
-      .text(function(d) { return formatNumber(d.value) })
-
-    g.append("text")
-      .attr("dy", ".75em")
-      .text(function(d) { return d.name })
-      .call(text)
-
-    function transition(d) {
-      if (transitioning || !d) return
-      transitioning = true
-      var g2 = display(d),
-          t1 = g1.transition().duration(750),
-          t2 = g2.transition().duration(750)
-
-      // Update the domain only after entering new elements.
-      x.domain([d.x, d.x + d.dx])
-      y.domain([d.y, d.y + d.dy])
-
-      // Enable anti-aliasing during the transition.
-      svg.style("shape-rendering", null)
-
-      // Draw child nodes on top of parent nodes.
-      svg.selectAll(".depth").sort(function(a, b) { return a.depth - b.depth })
-
-      // Fade-in entering text.
-      g2.selectAll("text").style("fill-opacity", 0)
-
-      // Transition to the new view.
-      t1.selectAll("text").call(text).style("fill-opacity", 0)
-      t2.selectAll("text").call(text).style("fill-opacity", 1)
-      t1.selectAll("rect").call(rect)
-      t2.selectAll("rect").call(rect)
-
-      // Remove the old node when the transition is finished.
-      t1.remove().each("end", function() {
-        svg.style("shape-rendering", "crispEdges")
-        transitioning = false
-      })
-    }
-
-    return g
-  }
-
-  function text(text) {
-    text.attr("x", function(d) { return x(d.x) + 6 })
-        .attr("y", function(d) { return y(d.y) + 6 })
-  }
-
-  function rect(rect) {
-    rect.attr("x", function(d) { return x(d.x) })
-        .attr("y", function(d) { return y(d.y) })
-        .attr("width", function(d) { return x(d.x + d.dx) - x(d.x) })
-        .attr("height", function(d) { return y(d.y + d.dy) - y(d.y) })
-  }
-
-  function name(d) {
-    return d.parent
-        ? name(d.parent) + "." + d.name
-        : d.name
-  }
-
-}) // d3.json
-
 }
 
 /*
